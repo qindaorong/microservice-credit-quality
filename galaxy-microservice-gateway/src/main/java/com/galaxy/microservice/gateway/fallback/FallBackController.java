536115c71e073a9ea7b2b3e6abb535c4c98cd6c6
qinrong726 <qindaorong@126.com>
2019-04-03 17:51:38 +0800

    fix bugs

---
+++ galaxy-microservice-gateway/src/main/java/com/galaxy/microservice/gateway/fallback/FallBackController.java
@@ -0,0 +1,21 @@
+package com.galaxy.microservice.gateway.fallback;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * @ClassName： FallBackController
+ * @Description
+ * @Author alan qin
+ * @Date 2019-04-03
+ **/
+@RestController
+public class FallBackController {
+
+    @RequestMapping("/gzt/fallback")
+    public Mono<String> gztFallback() {
+        return Mono.just("[gzt] service error, jump fallback");
+    }
+
+}
